52a10131c97aba701cb7b3a7fe620b2b761e6a28
Amrutha <anoopamrutha7.com>
2025-12-17 04:05:23 +0530

    Clean up: remove redundant documentation and build artifacts

--- mysql_setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS payment_collection_db;
-CREATE USER IF NOT EXISTS 'payment_user'@'localhost' IDENTIFIED BY 'PaymentApp2025!';
-GRANT ALL PRIVILEGES ON payment_collection_db.* TO 'payment_user'@'localhost';
-FLUSH PRIVILEGES;
